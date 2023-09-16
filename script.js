6aff76bf9bad617356c43f9246a491092978dae1
Juancho9811 <74477799+Juancho9811@users.noreply.github.com>
2023-09-16 16:00:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        if (index >= 0 && index < slides.length) {
+            slides.forEach((slide) => {
+                slide.classList.remove("active");
+            });
+    
+            slides[index].classList.add("active");
+        }
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Mostrar la primera imagen al cargar la página
+    showSlide(currentIndex);
+
+    // Cambiar automáticamente de imagen cada 3 segundos
+    setInterval(nextSlide, 3000);
+});
